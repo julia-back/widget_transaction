9699b9fb8638118049cd96524522eafe42dda8e7
Julia <yubengert@gmail.com>
2024-07-29 15:35:03 +0300

    git init

---
+++ src/masks.py
@@ -0,0 +1,13 @@
+def get_mask_card_number(card_num: int) -> str:
+    """Принимает номер карты и маскирует цифры, кроме первых шести и последних четырех цифр"""
+    mask_num = str(card_num)[:4] + " " + str(card_num)[4:6] + "** **** " + str(card_num)[-4:]
+    return mask_num
+
+
+def get_mask_account(account_num: int) -> str:
+    """Принимает номер счета и возвращает две звездочки и последние четыре цифры"""
+    return "**" + str(account_num)[-4:]
+
+
+print(get_mask_card_number(6666666666666666))
+print(get_mask_account(6666666666666666666666666666666666))
