c4e1e57a9665ad7d8e6c84a8a8e2b368d5ca66ac
Julia <yubengert@gmail.com>
2024-07-30 14:59:35 +0300

    fixed mega function mask_account_card

--- src/widget.py
+++ src/widget.py
@@ -8,15 +8,30 @@ def mask_account_card(kind_and_numbers: str) -> str:
             kind += symbol
         elif symbol.isdigit():
             numbers += symbol
-    if kind.lower() == "счет ":
+    # Очистим строку типа карты или счета от пробелов
+    kind_clean = ""
+    for letter in kind:
+        if letter.isalpha():
+            kind_clean += letter
+    # Добавим пробел перед большой, если тип карты или счета состоит из двух или более слов
+    kind_list = list(kind_clean)
+    i = 1
+    while i < len(kind_list):
+        if kind_list[i].isupper():
+            kind_list.insert(i, " ")
+            i += 1
+        i += 1
+        kind_clean = "".join(kind_list)
+    # Выведем результат в зависимости от типа карты или счета
+    if kind_clean.lower() == "счет":
         mask_account = masks.get_mask_account(int(numbers))
-        return kind + mask_account
+        return kind_clean + " " + mask_account
     else:
         mask_card = masks.get_mask_card_number(int(numbers))
-        return kind + mask_card
+        return kind_clean + " " + mask_card
 
 
-print(mask_account_card("Visa Platinum 8990922113665229"))
+print(mask_account_card("Visa   Platinum  Super Mega    Gold        7000 7922 8960 6361"))
 
 
 def get_date(complication_data: str) -> str:
