64c4efd49c515b7a96b12696387b38f719e8b2aa
Julia <yubengert@gmail.com>
2024-08-08 02:02:14 +0300

    fixed masks.py and test_masks.py

---
+++ tests/test_processing.py
@@ -0,0 +1,5 @@
+from src import processing
+
+
+processing.filter_by_state("")
+processing.sort_by_date("")
---
+++ tests/test_widget.py
@@ -0,0 +1,5 @@
+from src import widget
+
+
+widget.mask_account_card("")
+widget.get_date("")
