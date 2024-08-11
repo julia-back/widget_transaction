a6684e17f3cf5ae13cff9fa785f08382f139b079
Julia <yubengert@gmail.com>
2024-08-11 19:13:33 +0300

    add transactions_descriptions, add test_generators.py

--- src/generators.py
+++ src/generators.py
@@ -1,3 +1,15 @@
 def filter_by_currency(transactions, code):
+    """
+    Принимает на вход список словарей, представляющих транзакции, и код валюты для фильтрации, возвращает итератор,
+    который поочередно выдает транзакции, где валюта операции соответствует заданной (например, USD).
+    """
     filter_transactions = (t for t in transactions if t["operationAmount"]["currency"]["code"] == code)
     return filter_transactions
+
+
+def transaction_descriptions(transactions):
+    """Принимает список словарей с транзакциями и возвращает описание каждой операции по очереди"""
+    for transaction in transactions:
+        yield transaction["description"]
+
+
