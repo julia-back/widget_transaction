af7ff80da0a09d87313dceac580204b5ae0a7555
Julia <yubengert@gmail.com>
2024-08-13 17:51:23 +0300

    add decorator log in decorator.py

---
+++ src/decorators.py
@@ -0,0 +1,34 @@
+from functools import wraps
+from time import time
+
+
+def log(filename=None):
+    def wrapper(func):
+        @wraps
+        def inner(*args, **kwargs):
+            time_start = time()
+            try:
+                result = func(*args, **kwargs)
+            except Exception as error_info:
+                time_stop = time()
+                if filename is None:
+                    print(f"Function {func.__name__} started in {time_start}\n"
+                          f"Function {func.__name__} finished in {time_stop} by error:\n{error_info}\n"
+                          f"Parameters - args: {args}, kwargs: {kwargs}\n")
+                with open(filename, "a", encoding="utf-8"):
+                    filename.write(f"Function {func.__name__} started in {time_start}\n"
+                                   f"Function {func.__name__} finished in {time_stop} by error:\n{error_info}\n"
+                                   f"Parameters - args: {args}, kwargs: {kwargs}\n")
+            else:
+                time_stop = time()
+                if filename is None:
+                    print(f"Function {func.__name__} started in {time_start}\n"
+                          f"Function {func.__name__} finished in {time_stop}\n"
+                          f"Result: {result}\n")
+                with open(filename, "a", encoding="utf-8"):
+                    filename.write(f"Function {func.__name__} started in {time_start}\n"
+                                   f"Function {func.__name__} finished in {time_stop}\n"
+                                   f"Result: {result}\n")
+                return result
+        return inner
+    return wrapper
