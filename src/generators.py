9649b91655eb176c365488d77ee59aabdcd6c40c
Julia <yubengert@gmail.com>
2024-08-13 13:53:54 +0300

    fixed generators.py

--- src/generators.py
+++ src/generators.py
@@ -1,4 +1,4 @@
-def filter_by_currency(transactions, code):
+def filter_by_currency(transactions: list, code: str):
     """
     Принимает на вход список словарей, представляющих транзакции, и код валюты для фильтрации, возвращает итератор,
     который поочередно выдает транзакции, где валюта операции соответствует заданной (например, USD).
@@ -7,17 +7,17 @@ def filter_by_currency(transactions, code):
     return filter_transactions
 
 
-def transaction_descriptions(transactions):
+def transaction_descriptions(transactions: list) -> dict:
     """Принимает список словарей с транзакциями и возвращает описание каждой операции по очереди"""
     for transaction in transactions:
         yield transaction["description"]
 
 
-def card_number_generator(start, stop):
+def card_number_generator(start: int | str, stop) -> str:
     """
     Выдает номера банковских карт в формате XXXX XXXX XXXX XXXX, где X — цифра номера карты.
     Принимает начальное и конечное значения для генерации диапазона номеров.
-"""
-    for num in range(start, stop + 1):
+    """
+    for num in range(int(start), int(stop) + 1):
         nums = "0000000000000000" + str(num)
         yield nums[-16:-12] + " " + nums[-12:-8] + " " + nums[-8:-4] + " " + nums[-4:]
