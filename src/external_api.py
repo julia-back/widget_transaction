d8c6a4cf70290ef5bc182ac71451c7d522474324
Julia <yubengert@gmail.com>
2024-08-27 01:37:17 +0300

    add get_conversion_to_rubles in external_api.py

--- src/external_api.py
+++ src/external_api.py
@@ -1,3 +1,10 @@
+import json
+import os
+
+import dotenv
+import requests
+
+
 def get_conversion_to_rubles(transaction: dict[str]) -> float:
     """
     Принимает на вход транзакцию и возвращает сумму транзакции в рублях, тип данных — float.
@@ -9,16 +16,33 @@ def get_conversion_to_rubles(transaction: dict[str]) -> float:
     if currency == "RUB":
         return amount
     else:
-        pass
+        url = f"https://api.apilayer.com/exchangerates_data/convert?amount={amount}&from={currency}&to=RUB"
+        dotenv.load_dotenv()
+        api_key = os.getenv("API_KEY")
+        headers = {"apikey": api_key}
+        responce = requests.get(url, headers=headers)
+        with open("convert.json", "w", encoding="utf-8") as convert_file:
+            convert_file.write(responce.text)
+        with open("convert.json", "r", encoding="utf-8") as convert_file:
+            convert_dict = json.load(convert_file)
+            amount = convert_dict["result"]
+            return round(amount, 2)
+
+
 
 
 print(get_conversion_to_rubles({
-    "id": 441945886,
+    "id": 41428829,
     "state": "EXECUTED",
-    "date": "2019-08-26T10:50:58.294041",
+    "date": "2019-07-03T18:35:29.512364",
     "operationAmount": {
-      "amount": "31957.58",
+      "amount": "8221.37",
       "currency": {
-        "name": "руб.",
-        "code": "RUB"
-      }}}))
+        "name": "USD",
+        "code": "USD"
+      }
+    },
+    "description": "Перевод организации",
+    "from": "MasterCard 7158300734726758",
+    "to": "Счет 35383033474447895560"
+  }))
