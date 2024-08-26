0efe84ba69a8fdc192ba2b3c3e8b5038acd02360
Julia <yubengert@gmail.com>
2024-08-26 23:22:23 +0300

    add tests for get_read_json_file in test_utils

---
+++ tests/test_utils.py
@@ -0,0 +1,20 @@
+import json
+import os
+from unittest.mock import Mock, patch
+
+from main import PATH
+from src.utils import get_read_json_file
+
+
+def test_get_read_json_file_ok():
+    json.load = Mock(return_value=[{"1": "1"}, {"2": "2"}])
+    assert get_read_json_file(os.path.join(PATH, "data", "operations.json")) == [{"1": "1"}, {"2": "2"}]
+
+
+def test_get_read_json_file_not_found_file():
+    assert get_read_json_file("") == []
+
+
+def test_get_read_json_file_is_not_list():
+    json.load = Mock(return_value={})
+    assert get_read_json_file(os.path.join(PATH, "data", "operations.json")) == []
