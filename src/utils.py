cecbd6494d8bd8c8d53838f56da5b4e9f1e1369c
Julia <yubengert@gmail.com>
2024-08-28 16:05:35 +0300

    add logs for utils.py

--- src/utils.py
+++ src/utils.py
@@ -1,5 +1,16 @@
 import json
+import os
 import logging
+from main import PATH
+
+
+logging.basicConfig(filemode="w")
+logger = logging.getLogger("utils.py")
+handler = logging.FileHandler(os.path.join(PATH, "logs", "logs_utils.log"))
+formatter = logging.Formatter("%(asctime)s %(name)s %(levelname)s - %(message)s")
+handler.setFormatter(formatter)
+logger.addHandler(handler)
+logger.setLevel(logging.DEBUG)
 
 
 def get_read_json_file(json_file: str) -> list[dict[str, str]]:
@@ -7,16 +18,19 @@ def get_read_json_file(json_file: str) -> list[dict[str, str]]:
     Принимает на вход путь до JSON-файла и возвращает список словарей с данными о финансовых транзакциях.
     Если файл пустой, содержит не список или не найден, функция возвращает пустой список.
     """
+    logging.info(f"Start read file {json_file}")
     try:
+        logging.info(f"Opening file {json_file}")
         with open(json_file, "r", encoding="utf-8") as file:
             file_data = json.load(file)
-    except FileNotFoundError:
-        print("Файл не найден или указан некорректный путь")
+    except FileNotFoundError as ex:
+        logging.error(f"{ex}: file not found at this path")
         return []
-    except json.JSONDecodeError:
-        print("Ошибка декодирования файла")
+    except json.JSONDecodeError as ex:
+        logging.error("Error decoding file")
         return []
     else:
+        logging.info(f"Open file {json_file}")
         if type(file_data) is list:
             return file_data
         else:
