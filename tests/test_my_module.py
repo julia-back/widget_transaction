4d91efff6f0418e7cd95a04c9dd781d4ab31f6d7
Julia <yubengert@gmail.com>
2024-09-06 11:30:45 +0300

    fix test for get_search

--- tests/test_my_module.py
+++ tests/test_my_module.py
@@ -55,43 +55,10 @@ def transactions():
 
 def test_get_search_1(transactions):
     result = my_module.get_search(transactions, "сЧЕт")
-    assert result == [
-        {
-            "id": 441945886,
-            "state": "EXECUTED",
-            "date": "2019-08-26T10:50:58.294041",
-            "operationAmount": {"amount": "31957.58", "currency": {"name": "руб.", "code": "RUB"}},
-            "description": "Перевод организации",
-            "from": "Maestro 1596837868705199",
-            "to": "Счет 64686473678894779589",
-        },
-        {
-            "id": 587085106,
-            "state": "EXECUTED",
-            "date": "2018-03-23T10:45:06.972075",
-            "operationAmount": {"amount": "48223.05", "currency": {"name": "руб.", "code": "RUB"}},
-            "description": "Открытие вклада",
-            "to": "Счет 41421565395219882431",
-        },
-        {
-            "id": 142264268,
-            "state": "EXECUTED",
-            "date": "2019-04-04T23:20:05.206878",
-            "operationAmount": {"amount": "79114.93", "currency": {"name": "USD", "code": "USD"}},
-            "description": "Перевод со счета на счет",
-            "from": "Счет 19708645243227258542",
-            "to": "Счет 75651667383060284188",
-        },
-        {
-            "id": 594226727,
-            "state": "CANCELED",
-            "date": "2018-09-12T21:27:25.241689",
-            "operationAmount": {"amount": "67314.70", "currency": {"name": "руб.", "code": "RUB"}},
-            "description": "Перевод организации",
-            "from": "Visa Platinum 1246377376343588",
-            "to": "Счет 14211924144426031657",
-        },
-    ]
+    assert result == [{'id': 142264268, 'state': 'EXECUTED', 'date': '2019-04-04T23:20:05.206878',
+                       'operationAmount': {'amount': '79114.93', 'currency': {'name': 'USD', 'code': 'USD'}},
+                       'description': 'Перевод со счета на счет', 'from': 'Счет 19708645243227258542',
+                       'to': 'Счет 75651667383060284188'}]
 
 
 def test_get_search_2(transactions):
